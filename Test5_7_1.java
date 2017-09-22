ce3253ee0529d495206655d1ab173232654c6855
kimsc <kimsc1218@naver.com>
2017-09-22 10:31:44 +0900

    자바파일 김성철 2017-09-22-10:31

---
+++ Test5_7_1.java
@@ -0,0 +1,26 @@
+
+public class Test5_7_1 {
+
+	public static void main(String[] args) {
+		
+	
+		int i,j,k;
+		
+		
+		for(i=2;i<10;i+=2) {
+			System.out.println(i + "´Ü");
+			for(j=1;j<10;j++) {
+				k=i*j;
+				
+				System.out.print(i + "*" + j + "=" + k + " ");
+			}
+			System.out.println();
+			
+		}
+		
+		
+		 
+		
+	}
+
+}
